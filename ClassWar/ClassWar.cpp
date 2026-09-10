92678a7d60e93612f538f2c41561e9bcb0a49249
Admin <Admin@DESKTOP-4EEO4FB>
2026-09-10 12:25:43 +0300

    Add project files.

---
+++ ClassWar/ClassWar.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+using namespace std;
+
+class Weapon {
+protected:
+    string requiredOwner;
+
+public:
+    Weapon(string owner) : requiredOwner(owner) {}
+
+    virtual void useWeapon() const = 0;
+    virtual string getName() const = 0;
+
+    string getRequiredOwner() const {
+        return requiredOwner;
+    }
+    virtual ~Weapon() = default;
+};
