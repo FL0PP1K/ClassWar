781199fb9f5c4c0b65dbd4ca0f68365cdaa3c1ef
Admin <ostap.kovalskij@kpk-lp.com.ua>
2026-09-10 13:38:58 +0300

    Додано головний ігровий цикл main, меню та обробку введення

--- ClassWar/ClassWar.cpp
+++ ClassWar/ClassWar.cpp
@@ -127,3 +127,120 @@ class Troll : public Character {
 public:
     Troll() : Character("Троль", "Дика і страшна сила", "Троль") {}
 };
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    Character* player = nullptr;
+    bool running = true;
+    while (running) {
+        cout << " === ГОЛОВНЕ МЕНЮ === " << endl;
+        cout << "1. Вибрати персонажа" << endl;
+        cout << "2. Вибрати зброю" << endl;
+        cout << "3. Атакувати" << endl;
+        cout << "4. Вийти з гри" << endl;
+        cout << "Ваш вибір (1-4): ";
+        int mainChoice;
+        cin >> mainChoice;
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(32767, '\n');
+            cout << "\nВведіть цифру від 1 до 4." << endl;
+            continue;
+        }
+        switch (mainChoice) {
+        case 1: {
+            int charChoice;
+            cout << "\n === ВИБІР ПЕРСОНАЖА === " << endl;
+            cout << "1. Чарівник\n2. Лицар\n3. Ельф\n4. Троль\nВаш вибір: ";
+            cin >> charChoice;
+            if (cin.fail()) {
+                cin.clear();
+                cin.ignore(32767, '\n');
+                cout << "Невірне введення!" << endl;
+                break;
+            }
+            if (player != nullptr) {
+                delete player;
+                player = nullptr;
+            }
+            switch (charChoice) {
+            case 1: player = new Wizard(); break;
+            case 2: player = new Knight(); break;
+            case 3: player = new Elf(); break;
+            case 4: player = new Troll(); break;
+            default:
+                cout << "Ви стали Тролем." << endl;
+                player = new Troll();
+                break;
+            }
+            cout << "\n";
+            player->display();
+            break;
+        }
+        case 2: {
+            if (player == nullptr) {
+                cout << "\nСпочатку виберіть персонажа!" << endl;
+                break;
+            }
+            int weaponChoice;
+            cout << "\n === ВИБІР ЗБРОЇ === " << endl;
+            cout << "1. Магічний посох (Чарівник) 2. Гримуар Таємниць (Чарівник)\n";
+            cout << "3. Королівський меч (Лицар) 4. Важкий молот (Лицар)\n";
+            cout << "5. Ельфійський лук (Ельф) 6. Парні кинджали (Ельф)\n";
+            cout << "7. Величезна дубина (Троль) 8. Важкий валун (Троль)\n";
+            cout << "Ваш вибір (1-8): ";
+            cin >> weaponChoice;
+            if (cin.fail()) {
+                cin.clear();
+                cin.ignore(32767, '\n');
+                cout << "Невірне введення!" << endl;
+                break;
+            }
+            Weapon* selectedWeapon = nullptr;
+            switch (weaponChoice) {
+            case 1: selectedWeapon = new WizardStaff(); break;
+            case 2: selectedWeapon = new WizardGrimoire(); break;
+            case 3: selectedWeapon = new KnightSword(); break;
+            case 4: selectedWeapon = new KnightHammer(); break;
+            case 5: selectedWeapon = new ElfBow(); break;
+            case 6: selectedWeapon = new ElfDaggers(); break;
+            case 7: selectedWeapon = new TrollClub(); break;
+            case 8: selectedWeapon = new TrollBoulder(); break;
+            default: break;
+            }
+            if (selectedWeapon != nullptr) {
+                cout << "\n";
+                if (!player->equipWeapon(selectedWeapon)) {
+                    delete selectedWeapon;
+                }
+            }
+            else {
+                cout << "Невірний вибір зброї." << endl;
+            }
+            break;
+        }
+        case 3: {
+            if (player == nullptr) {
+                cout << "\nСпочатку виберіть персонажа!" << endl;
+            }
+            else {
+                cout << "\n === АТАКА === " << endl;
+                player->fight();
+            }
+            break;
+        }
+        case 4: {
+            cout << "\nВихід з гри. До зустрічі!" << endl;
+            running = false;
+            break;
+        }
+        default:
+            cout << "\nНевідома команда!" << endl;
+            break;
+        }
+    }
+    if (player != nullptr) {
+        delete player;
+    }
+    return 0;
+}
